3adc52fda2d91bc76c036a2a61f09c11ee15aaea
Giovani <desouzagiovani9@gmail.com>
2023-04-15 21:01:55 +0100

    Fixings bugs

--- pages/index.tsx
+++ pages/index.tsx
@@ -40,7 +40,7 @@ const Home: NextPage = () => {
         </div>
         
         <div className="w-full lg:w-3/4 flex flex-col items-center lg:flex-row my-6 lg:justify-around m-auto">
-          <div className=" flex justify-between items-center w-full sm:w-1/3  text-left ">
+          <div className=" flex justify-between items-center w-3/4 sm:w-1/3  text-left ">
             <div>
 <h2 className="flex leading-tight font-bold flex-col text-7xl dark:text-black text-white">Pictures <span className="text-[#fdd36a]">restored</span></h2>
             <p className="block dark:text-black text-white my-8 text-2xl ">With restorePhotos you can  restore  even small and vintages images in just a few seconds </p>
@@ -64,21 +64,21 @@ const Home: NextPage = () => {
         
         </div>
         <div className=" dark:border-black border-white w-[100%] p-12">
-           <h1 className="max-w-4xl dark:text-black leading-snug text-4xl lg:text-7xl  block m-auto font-bold text-white sm:text-center">
+           {/* <h1 className="max-w-4xl dark:text-black leading-snug text-4xl lg:text-7xl  block m-auto font-bold text-white sm:text-center">
           Any questions ?
-        </h1>
-        <p className="text-sm sm:text-xl text-white dark:text-black sm:text-center max-w-3xl mt-6 font-medium m-auto">
+        </h1> */}
+        {/* <p className="text-sm sm:text-xl text-white dark:text-black sm:text-center max-w-3xl mt-6 font-medium m-auto">
         Remini Mobile App and <span className="font-bold">Remini Web</span>  are two separate services with different subscriptions. If you want to know more about the two subscriptions, visit the FAQ section for the specific product.
         </p>
 
         <p className="sm:text-xl text-white lg:text-2xl dark:text-black sm:text-center max-w-3xl mt-6 font-bold pt-6 m-auto">
           Which subscription do you have ?
-        </p>
-        {/* <div className="py-8 w-1/2 dark:bg-black my-12 text-2xl m-auto flex items-center justify-between text-white rounded-2xl px-10 font-semibold bg-[#121216]">
+        </p> */}
+         {/* <div className="py-8 w-3/4 sm:w-1/2 dark:bg-black my-12 text-2xl m-auto flex items-center justify-between text-white rounded-2xl px-10 font-semibold bg-[#121216]">
        <span> RestorePhotos Mobile App</span>
        <button className="py-4 px-8 border-2 rounded-full">Read FAQ </button>
-          </div>
-          <div className="py-8 w-1/2 dark:bg-black  my-12 text-2xl m-auto flex items-center justify-between text-white rounded-2xl px-10  font-semibold bg-[#121216]">
+          </div> */}
+          {/*<div className="py-8 w-1/2 dark:bg-black  my-12 text-2xl m-auto flex items-center justify-between text-white rounded-2xl px-10  font-semibold bg-[#121216]">
        <span> RestorePhotos Mobile App</span>
           <button className="py-4 px-8 border-2 rounded-full">Read FAQ </button>
           </div> */}
